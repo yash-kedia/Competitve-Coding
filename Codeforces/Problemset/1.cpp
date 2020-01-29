2d7ba1ca69024e675da92507e3291c995e156e53
Yash Kedia <39669117+yash-kedia@users.noreply.github.com>
2020-01-29 19:56:05 +0530

    Create 1.cpp

---
+++ Codeforces/Problemset/1.cpp
@@ -0,0 +1,45 @@
+//link to question  https://codeforces.com/contest/1249/problem/B1
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        int ar[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+ 
+ 
+        }
+        for(int i=0;i<n;i++){
+ 
+            if(arr[i]!=i+1){
+                    int count=2;
+                    int temp=arr[arr[i]-1];
+                    //cout<<temp;
+                    while(temp!=i+1){
+                          //cout<<temp<<" ";
+                        temp=arr[temp-1];
+                        //cout<<temp<<endl;
+ 
+ 
+                        count++;
+                    }
+                    ar[i]=count;
+ 
+            }
+            else{
+                    ar[i]=1;
+            }
+ 
+            }
+ 
+        for(int i=0;i<n;i++){
+           cout<<ar[i]<<" ";
+        }
+        cout<<endl;
+    }
+ 
+}
