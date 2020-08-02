115a69ec07de7f3a6178cb3e1f7aa57619f92658
Yash Kedia <39669117+yash-kedia@users.noreply.github.com>
2020-08-03 04:42:56 +0530

    Create activityselection_greedy.cpp

---
+++ Advanced C++/activityselection_greedy.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct Pair{
+int start;
+int finish;
+};
+
+void activityselector(vector<Pair> const &vec){
+int k = 0;
+set<int> out;
+
+out.insert(0);
+
+for(int i = 1;i<vec.size();i++){
+
+        if(vec[i].start>= vec[k].finish){
+            out.insert(i);
+            k = i;
+        }
+
+}
+
+for(int i:out){
+    cout<<"{"<<vec[i].start<<" ,"<<vec[i].finish<<"}"<<'\n';
+}
+}
+int main(){
+
+
+vector<Pair> activities = {
+
+{1, 4}, {3, 5}, {0, 6}, {5, 7}, {3, 8}, {5, 9},
+		{6, 10}, {8, 11}, {8, 12}, {2, 13}, {12, 14}
+
+};
+
+sort(activities.begin(),activities.end(),[](auto const &lhs,auto const &rhs){
+     return lhs.finish<rhs.finish;});
+
+     activityselector(activities);
+
+
+}
